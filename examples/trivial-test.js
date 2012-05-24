e35a1e546c25c226f671efa3ca45d5fbdc99537b
Morten Siebuhr <sbhr@sbhr.dk>
2012-05-24 10:55:34 +0200

    Add a trivial example.

---
+++ examples/trivial-test.js
@@ -0,0 +1,11 @@
+var sdc = require('../lib/statsd-client'),
+    SDC = new sdc('10.111.12.113', {debug: 1, prefix: "statsd-client"});
+
+var begin = new Date();
+setTimeout(function () {
+    SDC.gauge('test.gauge', 100 * Math.random());
+    SDC.increment('test.counter');
+    SDC.timing('speed', begin);
+    SDC.close();
+}, 100 * Math.random());
+
