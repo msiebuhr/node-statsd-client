2c9077ad68f9cb1b58636c0d56f7e732efdfd385
Morten Siebuhr <sbhr@sbhr.dk>
2012-10-23 11:02:54 +0200

    Remove extraneous layer of nesting in unit-tests.

--- test/StatsDClient.js
+++ test/StatsDClient.js
@@ -15,89 +15,84 @@ describe('StatsDClient', function () {
         });
     });
 
-    describe('W. FakeServer', function () {
-        var s, c;
+    var s, c;
 
-        before(function (done) {
-            s = new FakeServer();
-            c = new StatsDClient();
-            s.start(done);
+    before(function (done) {
+        s = new FakeServer();
+        c = new StatsDClient();
+        s.start(done);
+    });
+
+    describe("Counters", function () {
+        it('.counter("abc", 1) → "abc:1|c', function (done) {
+            c.counter('abc', 1);
+            s.expectMessage('abc:1|c', done);
         });
 
-        describe("Counters", function () {
-            it('.counter("abc", 1) → "abc:1|c', function (done) {
-                c.counter('abc', 1);
-                s.expectMessage('abc:1|c', done);
-            });
-
-            it('.counter("abc", -5) → "abc:-5|c', function (done) {
-                c.counter('abc', -5);
-                s.expectMessage('abc:-5|c', done);
-            });
-
-            it('.increment("abc") → "abc:1|c', function (done) {
-                c.increment('abc');
-                s.expectMessage('abc:1|c', done);
-            });
-
-            it('.increment("abc", 10) → "abc:10|c', function (done) {
-                c.increment('abc', 10);
-                s.expectMessage('abc:10|c', done);
-            });
-
-            it('.decrement("abc", -2) → "abc:-2|c', function (done) {
-                c.decrement('abc', -2);
-                s.expectMessage('abc:-2|c', done);
-            });
-
-            it('.decrement("abc", 3) → "abc:-3|c', function (done) {
-                c.decrement('abc', -3);
-                s.expectMessage('abc:-3|c', done);
-            });
+        it('.counter("abc", -5) → "abc:-5|c', function (done) {
+            c.counter('abc', -5);
+            s.expectMessage('abc:-5|c', done);
         });
 
-        describe('Gauges', function () {
-            it('.gauge("gauge", 3) → "gauge:-3|g', function (done) {
-                c.gauge('gauge', 3);
-                s.expectMessage('gauge:3|g', done);
-            });
+        it('.increment("abc") → "abc:1|c', function (done) {
+            c.increment('abc');
+            s.expectMessage('abc:1|c', done);
         });
 
-        describe('Sets', function () {
-            it('.set("foo", 10) → "foo:10|s', function (done) {
-                c.set('foo', 10);
-                s.expectMessage('foo:10|s', done);
-            });
+        it('.increment("abc", 10) → "abc:10|c', function (done) {
+            c.increment('abc', 10);
+            s.expectMessage('abc:10|c', done);
         });
 
-        describe('Timers', function () {
-            it('.timing("foo", 10) → "foo:10|ms', function (done) {
-                c.timing('foo', 10);
-                s.expectMessage('foo:10|ms', done);
-            });
+        it('.decrement("abc", -2) → "abc:-2|c', function (done) {
+            c.decrement('abc', -2);
+            s.expectMessage('abc:-2|c', done);
+        });
 
-            it('.timing("foo", new Date(-20ms)) ~→ "foo:20|ms"', function (done) {
-                var d = new Date();
+        it('.decrement("abc", 3) → "abc:-3|c', function (done) {
+            c.decrement('abc', -3);
+            s.expectMessage('abc:-3|c', done);
+        });
+    });
+
+    describe('Gauges', function () {
+        it('.gauge("gauge", 3) → "gauge:-3|g', function (done) {
+            c.gauge('gauge', 3);
+            s.expectMessage('gauge:3|g', done);
+        });
+    });
+
+    describe('Sets', function () {
+        it('.set("foo", 10) → "foo:10|s', function (done) {
+            c.set('foo', 10);
+            s.expectMessage('foo:10|s', done);
+        });
+    });
+
+    describe('Timers', function () {
+        it('.timing("foo", 10) → "foo:10|ms', function (done) {
+            c.timing('foo', 10);
+            s.expectMessage('foo:10|ms', done);
+        });
+
+        it('.timing("foo", new Date(-20ms)) ~→ "foo:20|ms"', function (done) {
+            var d = new Date();
+            setTimeout(function () {
+                c.timing('foo', d);
+
                 setTimeout(function () {
-                    c.timing('foo', d);
+                    // Figure out if we git a right-looking message
+                    var sentMessages = s._packetsReceived;
+                    assert.lengthOf(sentMessages, 1);
+                    assert.match(
+                        sentMessages[0],
+                        /foo:2\d\|ms/
+                    );
 
-                    setTimeout(function () {
-                        // Figure out if we git a right-looking message
-                        var sentMessages = s._packetsReceived;
-                        assert.lengthOf(sentMessages, 1);
-                        assert.match(
-                            sentMessages[0],
-                            /foo:2\d\|ms/
-                        );
-
-                        // Expect it anyway, as we need to clean up the packet list.
-                        s.expectMessage(sentMessages[0], done);
-                    }, 10);
-                }, 20);
-            });
-
-
-
+                    // Expect it anyway, as we need to clean up the packet list.
+                    s.expectMessage(sentMessages[0], done);
+                }, 10);
+            }, 20);
         });
     });
 });
