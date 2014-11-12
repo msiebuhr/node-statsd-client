939dc7bba043ea1696aa97f43014c2b4570dc213
Raynos <raynos2@gmail.com>
2014-11-11 17:04:40 -0800

    add tests for the socket.

---
+++ test/socket.js
@@ -0,0 +1,129 @@
+var test = require('tape');
+var dgram = require('dgram');
+var setTimeout = require('timers').setTimeout;
+
+var EphemeralSocket = require('../lib/EphemeralSocket.js');
+
+var PORT = 8125;
+
+test('creates a socket', function t(assert) {
+    var sock = new EphemeralSocket({
+        host: 'localhost',
+        port: PORT
+    });
+
+    assert.equal(typeof sock.close, 'function');
+    assert.equal(typeof sock.send, 'function');
+
+    sock.close();
+    assert.end();
+});
+
+test('can write to socket', function t(assert) {
+    var server = UDPServer({ port: PORT }, function onBound() {
+        var sock = new EphemeralSocket({
+            host: 'localhost',
+            port: PORT
+        });
+
+        server.once('message', onMessage);
+        sock.send('hello');
+
+        function onMessage(msg) {
+            var str = String(msg);
+            assert.equal(str, 'hello');
+
+            sock.close();
+            server.close();
+            assert.end();
+        }
+    });
+});
+
+test('has default ports & hosts', function t(assert) {
+    var server = UDPServer({ port: PORT }, function onBound() {
+        var sock = new EphemeralSocket();
+
+        server.once('message', onMessage);
+        sock.send('hello');
+
+        function onMessage(msg) {
+            var str = String(msg);
+            assert.equal(str, 'hello');
+
+            sock.close();
+            server.close();
+            assert.end();
+        }
+    });
+});
+
+test('can send multiple packets', function t(assert) {
+    var server = UDPServer({ port: PORT }, function onBound() {
+        var sock = new EphemeralSocket({
+            host: 'localhost',
+            port: PORT
+        });
+        var messages = [];
+
+        server.on('message', onMessage);
+        sock.send('hello');
+        sock.send(' ');
+        sock.send('world');
+
+        function onMessage(msg) {
+            messages.push(String(msg));
+
+            if (messages.length === 3) {
+                onEnd();
+            }
+        }
+
+        function onEnd() {
+            // UDP is unordered messages
+            var str = messages.sort().join('');
+            assert.equal(str, ' helloworld');
+
+            sock.close();
+            server.close();
+            assert.end();
+        }
+    });
+});
+
+test('socket will close and timeout', function t(assert) {
+    var server = UDPServer({ port: PORT }, function onBound() {
+        var sock = new EphemeralSocket({
+            host: 'localhost',
+            port: PORT,
+            socket_timeout: 10
+        });
+
+        server.once('message', onMessage);
+        sock.send('hello');
+
+        function onMessage(msg) {
+            var str = String(msg);
+            assert.equal(str, 'hello');
+
+            setTimeout(expectClosed, 50);
+        }
+
+        function expectClosed() {
+            assert.equal(sock._socket, undefined);
+
+            server.close();
+            assert.end();
+        }
+    });
+});
+
+function UDPServer(opts, onBound) {
+    opts = opts || {};
+    var port = opts.port || PORT;
+
+    var server = dgram.createSocket('udp4');
+    server.bind(port, onBound);
+
+    return server;
+}
