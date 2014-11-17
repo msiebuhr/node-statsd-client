b44aefaee1727afc88a1e4c5dcd062b1b7a9853a
Raynos <raynos2@gmail.com>
2014-11-16 23:46:27 -0800

    Revert "Merge pull request #6 from uber/0.0.4-dns-recovery.cleanup-timeout"
    This reverts commit edad747e76c7c63a63d9779d7e09fa604f174725, reversing
    changes made to 6888dd75c055a313663cf0649c177da2dfd7a241.
    
    Conflicts:
    	lib/EphemeralSocket.js
    	test/socket.js

--- test/socket.js
+++ test/socket.js
@@ -93,7 +93,7 @@ test('can send multiple packets', function t(assert) {
     });
 });
 
-test('socket will unref', function t(assert) {
+test('socket will close and timeout', function t(assert) {
     var server = UDPServer({ port: PORT }, function onBound() {
         var sock = new EphemeralSocket({
             host: 'localhost',
@@ -109,7 +109,12 @@ test('socket will unref', function t(assert) {
             var str = String(msg);
             assert.equal(str, 'hello');
 
-            sock._queue.destroy();
+            setTimeout(expectClosed, 50);
+        }
+
+        function expectClosed() {
+            assert.equal(sock._socket, undefined);
+
             server.close();
             assert.end();
         }
