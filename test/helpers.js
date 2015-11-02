34b44ca53a1e50587b67b74d49f86f16f9194596
Morten Siebuhr <sbhr@sbhr.dk>
2015-11-02 10:09:58 +0100

    Tests: Some quick tests of express middleware helper functions.

--- test/helpers.js
+++ test/helpers.js
@@ -1,5 +1,5 @@
 var StatsDClient = require('../lib/statsd-client'),
-    EventEmitter = require('events').EventEmitter,
+    getExpressMiddlewareHelper = require('../lib/helpers/getExpressMiddleware'),
     assert = require('chai').assert;
 
 /*global describe before it*/
@@ -19,5 +19,66 @@ describe('Helpers', function () {
         var f = c.helpers.getExpressMiddleware('prefix');
         assert.isFunction(f);
         assert.lengthOf(f, 3);
+    });
+
+    describe('.getExpressMiddleware()', function () {
+        describe('.sanitize', function () {
+            it('Removes colons', function () {
+                assert.equal(
+                    getExpressMiddlewareHelper._sanitizeUrl(':colon:'),
+                    'colon'
+                );
+            });
+
+            it('Removes leding+trailing slashes', function () {
+                assert.equal(
+                    getExpressMiddlewareHelper._sanitizeUrl('/foo/'),
+                    'foo'
+                );
+            });
+
+            it('Replaces slashes with underscores', function () {
+                assert.equal(
+                    getExpressMiddlewareHelper._sanitizeUrl('foo/bar/baz'),
+                    'foo_bar_baz'
+                );
+            });
+        });
+
+        describe('.findRouteName', function () {
+            it('Returns .locals.statsdUrlKey, if set', function () {
+                assert.equal(
+                    getExpressMiddlewareHelper._findRouteName(
+                        {}, { locals: {statsdUrlKey: 'foobar'} }
+                    ), 'foobar');
+            });
+
+            it('Returns `root` for URL /', function () {
+                assert.equal(
+                    getExpressMiddlewareHelper._findRouteName(
+                        {
+                            route: { path: '/' },
+                            method: 'GET'
+                        }, {}
+                    ), 'GET_root');
+            });
+
+            it('Does what on no path???', function () {
+                assert.equal(
+                    getExpressMiddlewareHelper._findRouteName(
+                        { method: 'GET' }, {}
+                    ), undefined);
+            });
+
+            it('Returns regex souce on regex path', function () {
+                assert.equal(
+                    getExpressMiddlewareHelper._findRouteName(
+                        {
+                            method: 'GET',
+                            route: { path: /foo(\w+)bar/ }
+                        }, {}
+                    ), 'GET_foo(\\w+)bar');
+            });
+        });
     });
 });
